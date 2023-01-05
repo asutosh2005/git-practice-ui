531e7ee26ec8ca2678f99682c59b5393a8402934
keerti <keerti.pathak@thrymr.net-at-175029991794>
2023-01-05 09:13:38 +0530

    create kee component

---
+++ src/app/kee/kee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kee',
+  templateUrl: './kee.component.html',
+  styleUrls: ['./kee.component.scss']
+})
+export class KeeComponent {
+
+}
