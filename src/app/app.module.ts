d9e0f56f3433f4435fd9502ef8facd0e7066e71f
@bhishekOjha <abhishek.ojha@thrymr.net>
2023-01-18 10:01:33 +0530

    resolve merge conflicts in master branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-=======
 import { LoginComponent } from './login/login.component';
->>>>>>> c52884cc07851b5c24e5e75149f5019bfda05c24
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-    PageNotFoundComponent
-=======
+
+    PageNotFoundComponent,
+
     LoginComponent
->>>>>>> c52884cc07851b5c24e5e75149f5019bfda05c24
->>>>>>> 56a5da41cca46f59c0f65616bdde0a5e02f81c0d
+
   ],
   imports: [
     BrowserModule,
